e74107e51b9e9d2077de5570c216777ae91bfc13
IlyaZadonskyASCON <zadonskii_ia@ascon.ru>
2025-10-19 22:14:22 +0700

    Анимация пульсации сердца

--- FriendSoya/MainWindow.xaml.cs
+++ FriendSoya/MainWindow.xaml.cs
@@ -5,6 +5,7 @@ using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
@@ -16,9 +17,15 @@ namespace FriendSoya
     /// </summary>
     public partial class MainWindow : Window
     {
+        ScaleTransform _scaleTransform = new ScaleTransform(1.0, 1.1);
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Grid_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            DragMove();
         }
     }
 }
