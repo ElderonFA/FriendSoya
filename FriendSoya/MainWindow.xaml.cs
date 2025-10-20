875cf32ef8b7c77072f6ef8d05e05018d5cbf4c5
IlyaZadonskyASCON <zadonskii_ia@ascon.ru>
2025-10-21 01:12:22 +0700

    Добавил анимацию по клику

--- FriendSoya/MainWindow.xaml.cs
+++ FriendSoya/MainWindow.xaml.cs
@@ -1,31 +1,72 @@
-﻿using System.Text;
+﻿using FriendSoya.Scripts;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace FriendSoya
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
-        ScaleTransform _scaleTransform = new ScaleTransform(1.0, 1.1);
+        private bool _isInReactOnClick;
+
         public MainWindow()
         {
             InitializeComponent();
+            CreateAndPlayAnimHeartIdle();
         }
 
         private void Grid_MouseDown(object sender, MouseButtonEventArgs e)
         {
+            //Перемещение окна
             DragMove();
+        }
+
+        private async void Grid_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+            if (!_isInReactOnClick)
+                await PlayReactAnim(2);
+        }
+
+        private async Task PlayReactAnim(double seconds)
+        {
+            _isInReactOnClick = true;
+            var miliseconds = (int)(seconds * 1000);
+            
+            CreateAndPlayAnimHeartOnClickReact();
+
+            await Task.Delay(miliseconds);
+
+            _isInReactOnClick = false;
+            CreateAndPlayAnimHeartIdle();
+        }
+
+        private void CreateAndPlayAnimHeartIdle()
+        {
+            gjinImage.Source = new BitmapImage(new Uri("pack://application:,,,/Images/gjinIdle.png"));
+
+            //Анимация изменения размера
+            var pulseSizeHeartAnim = AnimationCreator.CreateScaleDoubleAnim(0.8, 1.3, TimeSpan.FromSeconds(2), true, true);
+            //Анимация изменения цвета
+            var pulseColorHeartAnim = AnimationCreator.CreateColorDoubleAnim(Colors.White, TimeSpan.FromSeconds(2), true, true);
+
+            heartScaleTransform.BeginAnimation(ScaleTransform.ScaleXProperty, pulseSizeHeartAnim);
+            heartScaleTransform.BeginAnimation(ScaleTransform.ScaleYProperty, pulseSizeHeartAnim);
+            heartGradientColor.BeginAnimation(GradientStop.ColorProperty, pulseColorHeartAnim);
+        }
+
+        private void CreateAndPlayAnimHeartOnClickReact()
+        {
+            gjinImage.Source = new BitmapImage(new Uri("pack://application:,,,/Images/gjinSmile.png"));
+
+            //Анимация изменения размера
+            var pulseSizeHeartAnim = AnimationCreator.CreateScaleDoubleAnim(0.8, 1.5, TimeSpan.FromSeconds(0.5), true, true);
+            //Анимация изменения цвета
+            var pulseColorHeartAnim = AnimationCreator.CreateColorDoubleAnim(Colors.LightGoldenrodYellow, TimeSpan.FromSeconds(0.5), true, true);
+
+            heartScaleTransform.BeginAnimation(ScaleTransform.ScaleXProperty, pulseSizeHeartAnim);
+            heartScaleTransform.BeginAnimation(ScaleTransform.ScaleYProperty, pulseSizeHeartAnim);
+            heartGradientColor.BeginAnimation(GradientStop.ColorProperty, pulseColorHeartAnim);
         }
     }
 }
---
+++ FriendSoya/Scripts/AnimationCreator.cs
@@ -0,0 +1,44 @@
+﻿using System.Windows.Media;
+using System.Windows.Media.Animation;
+
+namespace FriendSoya.Scripts
+{
+    public static class AnimationCreator
+    {
+        public static DoubleAnimation CreateScaleDoubleAnim(double from, double to, TimeSpan duration, bool reverse, bool isForeverAnim = false)
+        {
+            var anim = new DoubleAnimation
+            {
+                From = from,
+                To = to,
+                Duration = duration,
+                AutoReverse = reverse,
+            };
+
+            if (isForeverAnim)
+            {
+                anim.RepeatBehavior = RepeatBehavior.Forever;
+            }
+
+            return anim;
+        }
+
+        public static ColorAnimation CreateColorDoubleAnim(Color toColor, TimeSpan duration, bool reverse, bool isForeverAnim = false)
+        {
+            var anim = new ColorAnimation
+            {
+                To = toColor,
+                Duration = duration,
+                AutoReverse = reverse,
+                RepeatBehavior = RepeatBehavior.Forever
+            };
+
+            if (isForeverAnim)
+            {
+                anim.RepeatBehavior = RepeatBehavior.Forever;
+            }
+
+            return anim;
+        }
+    }
+}
